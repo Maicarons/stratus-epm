c7f3169558d82b94f724f2db0a2fd015016a2588
Maicarons <136282328+Maicarons@users.noreply.github.com>
2026-09-22 00:11:49 +0800

    feat(core): export public engine API

---
+++ packages/core/src/index.ts
@@ -0,0 +1,7 @@
+export * from './org/tree.js';
+export * from './fx/translate.js';
+export * from './consolidation/engine.js';
+export * from './budget/engine.js';
+export * from './allocation/engine.js';
+export * from './report/management.js';
+export * from './sample/demo.js';
